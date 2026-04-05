f681f9b336611409bb282fda8084c1929fb4a0f0
Hiepcdit <hiepnd.rtbed@gmail.com>
2026-04-05 20:59:40 +0700

    update not jpg

--- HDR/jsx/autoAction.jsx
+++ HDR/jsx/autoAction.jsx
@@ -1,32 +1,30 @@
-// (function main() {
-//     //list pathitems
-//     var lispath = new Array();
-//     var lengthPath = activeDocument.pathItems.length;
-//     deselectPath();
-//     for (var i = 0; i <= lengthPath - 1; i++) {
-//         var namePath = activeDocument.pathItems[i].name;
-//         doc.pathItems.getByName(namePath).makeSelection(0, true, SelectionType.REPLACE);
-//         var keywordsWall = ["tuong", "tường", "wall", "walls"];
-//         var keywordsTran = ["tran", "trần", "chi", "chỉ", "phao", "phào"];
-//         var keywordsWindow = ["cua", "cửa", "window", "windows"];
-//         // Danh sách cấu hình: [keywords, fileName]
-//         var checks = [
-//             [keywordsTran, "Whitening.jsx"],
-//             [keywordsWall, "wallColor.jsx"],
-//             [keywordsWindow, "insertWindow.jsx"]
-//         ];
-// 
-//         // Duyệt qua từng cấu hình
-//         for (var j = 0; j < checks.length; j++) {
-//             if (checkString(namePath, checks[j][0]) == true) {
-//                 // alert(namePath);
-//                 var targetScript = File(currentFolder + "/" + checks[j][1]);
-//                 $.evalFile(targetScript);
-//                 break; // chạy xong thì thoát
-//             }
-//         }
-//     }
-// })();
-// 
+(function main() {
+    //list pathitems
+    var lispath = new Array();
+    var lengthPath = activeDocument.pathItems.length;
+    deselectPath();
+    for (var i = 0; i <= lengthPath - 1; i++) {
+        var namePath = activeDocument.pathItems[i].name;
+        doc.pathItems.getByName(namePath).makeSelection(0, true, SelectionType.REPLACE);
+        var keywordsWall = ["tuong", "tường", "wall", "walls"];
+        var keywordsTran = ["tran", "trần", "chi", "chỉ", "phao", "phào"];
+        var keywordsWindow = ["cua", "cửa", "window", "windows"];
+        // Danh sách cấu hình: [keywords, fileName]
+        var checks = [
+            [keywordsTran, "Whitening.jsx"],
+            [keywordsWall, "wallColor.jsx"],
+            [keywordsWindow, "insertWindow.jsx"]
+        ];
 
-eval("@JSXBIN@ES@2.1@MyBbyBn0ABJgcnAENyBnAMAbyBn0AEJCnASzHjMjJjTjQjBjUjIBAEjzFiBjSjSjBjZCfntnftJDnASzKjMjFjOjHjUjIiQjBjUjIDBXzGjMjFjOjHjUjIEfXzJjQjBjUjIiJjUjFjNjTFfjzOjBjDjUjJjWjFiEjPjDjVjNjFjOjUGfnftJEnAEjzMjEjFjTjFjMjFjDjUiQjBjUjIHfnfaFbGn0AHJGnASzIjOjBjNjFiQjBjUjIIDXzEjOjBjNjFJfQzAKfXFfjGfVzBjJLfCnftJHnAEXzNjNjBjLjFiTjFjMjFjDjUjJjPjOMfEXzJjHjFjUiCjZiOjBjNjFNfXFfjzDjEjPjDOfRBVIfDffRDFdAFctXzHiSiFiQiMiBiDiFPfjzNiTjFjMjFjDjUjJjPjOiUjZjQjFQfffJInASzMjLjFjZjXjPjSjEjTiXjBjMjMREAREFeFjUjVjPjOjHFeFjU2lQB2mdgejOjHFeEjXjBjMjMFeFjXjBjMjMjTfnftJJnASzMjLjFjZjXjPjSjEjTiUjSjBjOSFARGFeEjUjSjBjOFeEjUjS2lHgejOFeDjDjIjJFeDjDjI2mJgeFeEjQjIjBjPFeEjQjInAjPfnftJKnASzOjLjFjZjXjPjSjEjTiXjJjOjEjPjXTGAREFeDjDjVjBFeDjD2nNgejBFeGjXjJjOjEjPjXFeHjXjJjOjEjPjXjTfnftJMnASzGjDjIjFjDjLjTUHARDARCVSfFFeNiXjIjJjUjFjOjJjOjHhOjKjTjYfARCVRfEFeNjXjBjMjMiDjPjMjPjShOjKjTjYfARCVTfGFeQjJjOjTjFjSjUiXjJjOjEjPjXhOjKjTjYffnftaTbyUn0ABOUbWn0ADJWnASzMjUjBjSjHjFjUiTjDjSjJjQjUVJEjzEiGjJjMjFWfRBCzBhLXCXjzNjDjVjSjSjFjOjUiGjPjMjEjFjSYfnneBhPXzBhRZfQKfVUfHVzBjKgafInnffnftJXnAEXzIjFjWjBjMiGjJjMjFgbfjzBhEgcfRBVVfJffDYnAKtACzChdhdgdEjzLjDjIjFjDjLiTjUjSjJjOjHgefRCVIfDXzBhQgffQKfVUfHVgafIffnnctnAVgafIAXEfVUfHByBzBhchAAVLfCACzBhNhBVDfBnndBByBzChchdhCAKB40BiAD4B0AiAL4C0AiAI4D0AiAR4E0AiAS4F0AiAT4G0AiAU4H0AiAga4I0AiAV4J0AiAAKAzEjNjBjJjOhDCgcnf0DKByB");
+        // Duyệt qua từng cấu hình
+        for (var j = 0; j < checks.length; j++) {
+            if (checkString(namePath, checks[j][0]) == true) {
+                // alert(namePath);
+                var targetScript = File(currentFolder + "/" + checks[j][1]);
+                $.evalFile(targetScript);
+                break; // chạy xong thì thoát
+            }
+        }
+    }
+})();
+
